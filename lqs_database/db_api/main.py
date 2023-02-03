20aa74d0d1d51bfebe35be77a304e5847457cfbc
omar elsayd <oelsayd@my.gcu.edu>
2023-02-03 16:21:45 -0700

    LQS-5: FASTAPI router

--- lqs_database/db_api/main.py
+++ lqs_database/db_api/main.py
@@ -1,8 +1,14 @@
 import logging
-
+from . import login
 from fastapi import FastAPI
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("FastAPI app")
 
-app = FastAPI()
+app = FastAPI(
+    title="LQS Database API",
+    description="API for LQS Database",
+    version="1.0.0",
+)
+
+app.include_router(login.router)
